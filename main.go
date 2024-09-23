29d42610b27529cfd27520c002756b336099662f
zhangzan <zhangzan@tal.com>
2024-09-23 18:12:29 +0800

    fet:jenkins 测试,github回调测试

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	fmt.Println("start jenkins job test ======> 睡眠5秒 准备执行Multiplexing方法")
+	fmt.Println("======> 测试github回调函数")
 	time.Sleep(5 * time.Second)
 	code.Multiplexing()
 
