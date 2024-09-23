eb1de56aaef1b12fc44013ec9b60ba2a3cd8e33c
zhangzan <zhangzan@tal.com>
2024-09-23 15:41:40 +0800

    fet:jenkins 测试

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "Multiplexing_/code"
+import (
+	"Multiplexing_/code"
+	"fmt"
+)
 
 func main() {
+	fmt.Println("start jenkins job test1")
 	code.Multiplexing()
 	//code.Kubernetes()
 
